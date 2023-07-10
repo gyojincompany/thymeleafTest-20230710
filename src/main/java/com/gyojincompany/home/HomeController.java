c6a36eee6c916d43384fb164eb3f84b5fb8f5ca6
gyojincompany <luckyred@gmail.com>
2023-07-10 14:19:00 +0900

    thymeleaf 기본기능 및 반복문 예제

---
+++ src/main/java/com/gyojincompany/home/HomeController.java
@@ -0,0 +1,55 @@
+package com.gyojincompany.home;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.gyojincompany.home.dto.MemberDto;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping(value = "/hello")
+	public String hello(Model model) {
+		
+		model.addAttribute("text", "HelloWorld!!");
+		
+		return "helloworld";
+	}
+	
+	@RequestMapping(value = "/member")
+	public String member(Model model) {
+		
+		MemberDto memberDto = new MemberDto("tiger", "홍길동", "tiger@abc.com", 27);
+		
+		model.addAttribute("memberDto", memberDto);
+		
+		return "member";
+	}
+	
+	@RequestMapping(value = "/memberList")
+	public String memberlist(Model model) {
+		
+		List<MemberDto> memberDtos = new ArrayList<MemberDto>();
+		
+		for(int i=1;i<=10;i++) {
+			MemberDto memberDto = new MemberDto();
+			memberDto.setId("tiger"+i);
+			memberDto.setName("홍길동"+i);
+			memberDto.setEmail("tiger"+i+"@abc.com");
+			memberDto.setAge(i+20);
+			
+			memberDtos.add(memberDto);
+		}
+		
+		model.addAttribute("memberDtos", memberDtos);
+		
+		return "memberlist";		
+	}
+	
+
+}
---
+++ src/main/java/com/gyojincompany/home/ThymeleafTest20230710Application.java
@@ -0,0 +1,13 @@
+package com.gyojincompany.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafTest20230710Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafTest20230710Application.class, args);
+	}
+
+}
